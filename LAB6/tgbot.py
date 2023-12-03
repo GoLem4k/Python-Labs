57a41f149ee0b8fa3faf3944fc44c94abaeccdef
Demid <demid09.2004@gmail.com>
2023-12-03 16:35:02 +0300

    1

---
+++ LAB6/tgbot.py
@@ -0,0 +1,22 @@
+import telebot as tg
+
+API_TOKEN = '6765338594:AAFYh5tTwdiLoD3bM3YQwBszj03p0kK9RDU'
+
+bot = tg.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Здарова лох\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
